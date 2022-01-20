b7e8383065b3e06c7bf95ff37beaf747ee4c0338
dror bahat <bahat.dror@gmail.com>
2022-01-20 17:37:40 +0200

    init

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,37 @@
+import { useState } from "react"
+
 const Home = () => {
+    const handleRequestAllTodos = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(res => res.json())
+            .then(data => {
+                setTodoList(data)
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }
+
+    const [todoList, setTodoList] = useState([])
+
     return (
         <div className="Home-root">
             <br />
-            <h1>home</h1>
-            <p>Welcome to my blog project!</p>
-        </div>
+            <h1>todo lost</h1>
+            <button
+                onClick={handleRequestAllTodos}>show list</button>
+            <ul>
+                {
+                    todoList.map(element => {
+                        return <li style={{ 
+                            textDecoration: element.completed ? 'line-through' : '',
+                            color: element.id % 2 === 0 ? 'red' : ''
+                        }}
+                            key={`todo-item-${element.id}`}>{element.title}</li>
+                    })
+                }
+            </ul>
+        </div >
     )
 }
 
