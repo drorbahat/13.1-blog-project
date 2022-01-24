124c33f85d101d79eb82eafd45e37e39934c110f
dror bahat <bahat.dror@gmail.com>
2022-01-24 21:11:14 +0200

    workflow finished

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom"
 
+
+
 const BlogItem = ({ blog }) => {
 
+    const handleDeleteBlog = () => {
+        fetch(`http://localhost:8000/blogs/${blog.id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(window.location.reload(false))
+    }
 
     return (
         <div className="BlogItem-root" id={`blog-item-${blog.id}`}>
-            <Link to={`/blogs/${blog.id}`}>
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
-            </Link>
+            <div>
+                <Link to={`/blogs/${blog.id}`}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </Link>
+            </div>
+            <button onClick={handleDeleteBlog} className="delete-button">delete</button>
         </div>
     )
 }
--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -1,33 +1,75 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 const CreateBlog = () => {
+
+    const navigate = useNavigate()
+
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [body, setBody] = useState('')
+
+    const saveNewBlog = e => {
+        e.preventDefault()
+        const prepairedBody = { title, body, author }
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(prepairedBody)
+        })
+            .then(() => {
+                setTitle('')
+                setAuthor('')
+                setBody('')
+                navigate('/home')
+            })
+            .catch(e => console.error(e))
+    }
+
+
     return (
         <div className="CreateBlog-root">
             <br />
             <h1>CreateBlog</h1>
             <div className="CreatBlog-main-container">
                 <form className="CreatBlog-form">
-                    <label htmlFor="title">title:</label>
+                    <label className="createBlog-lable" htmlFor="blog-title">title:</label>
                     <br />
                     <input
                         type="text"
                         id="blog-title"
-                        required />
+                        required
+                        value={title}
+                        onChange={e => { setTitle(e.target.value) }}
+                    />
                     <br /> <br />
-                    <label htmlFor="author">author:</label>
+                    <label className="createBlog-lable" htmlFor="blog-author">author:</label>
                     <br />
                     <input
                         type="text"
                         id="blog-author"
-                        required />
+                        required
+                        value={author}
+                        onChange={e => { setAuthor(e.target.value) }}
+                    />
                     <br /><br />
-                    <label htmlFor="post-body">enter post body:</label>
+                    <label className="createBlog-lable" htmlFor="blog-body">enter post body:</label>
                     <br />
                     <textarea
-                        name="blog-body" id="blog-body" cols="30" rows="10" required></textarea>
+                        name="blog-body"
+                        id="blog-body"
+                        cols="30"
+                        rows="10"
+                        required
+                        value={body}
+                        onChange={e => { setBody(e.target.value) }}
+                    ></textarea>
                     <br /> <br />
-                    <button type="submit">Submit</button>
+                    <button onClick={saveNewBlog} className="createBlog-submit-button" type="submit">Submit</button>
                 </form>
             </div>
-
         </div>
     )
 }
